32c043237839a3d6a00468cf752e3226715c7649
Nils Meijer <meijernils9@gmail.com>
2022-05-20 12:07:01 +0200

    Working on shop

---
+++ VR_FPSgame/Assets/ShopItem.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShopItem : MonoBehaviour
+{
+    private UnityEvent<Weapons> onPlayerAttemptBuy = new UnityEvent<Weapons>();
+    [SerializeField] private Weapons weaponType;
+    private BoxCollider boxTrigger;
+    
+    private void Start()
+    {
+        onPlayerAttemptBuy.AddListener(ShopManager.Instance.CheckTransaction);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if(other.CompareTag("PlayerHand")) onPlayerAttemptBuy.Invoke(weaponType);
+    }
+}
